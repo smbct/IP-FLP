61a0ef26432076bd0cf0576ecd330b55a6666c1e
smbct <smbct.dev@yahoo.com>
2016-12-20 17:03:47 +0100

    tabu search first version

--- src/tabu.c
+++ src/tabu.c
@@ -15,10 +15,14 @@ void rechercheTabu(Solution* meilleure) {
 
     // construction gloutonne
     construction(meilleure);
+
+    printf("solution gloutonne : \n");
     afficherSolution(meilleure);
+    printf("\n\n");
 
     Solution sol;
     creerSolution(meilleure->pb, &sol);
+    copierSolution(meilleure, &sol);
 
     // construction de la liste tabu
     int** tabuListe = malloc((long unsigned int)sol.pb->n*sizeof(int*));
@@ -32,15 +36,22 @@ void rechercheTabu(Solution* meilleure) {
     int longueurTabu = 7;
 
     int nbIt = 0;
-    int nbItMax = 100;
+    int nbItMax = 1000;
 
     while(nbIt < nbItMax) {
 
+        selectionnerVoisin(&sol, meilleure, tabuListe, nbIt, longueurTabu);
 
+        // si la solution actuelle est meilleure que la meilleure trouvée, mise à jour
+        if(sol.z < meilleure->z) {
+            copierSolution(&sol, meilleure);
+            nbItMax += 1000;
+        }
 
         nbIt ++;
     }
 
+    afficherSolution(meilleure);
 
     detruireSolution(&sol);
 
@@ -48,4 +59,72 @@ void rechercheTabu(Solution* meilleure) {
         free(tabuListe[i]);
     }
     free(tabuListe);
+}
+
+//------------------------------------------------------------------------------
+void selectionnerVoisin(Solution* sol, Solution* meilleure, int** tabuListe, int it, int longueurTabu) {
+
+    // voisinage : modification d'une connexion
+    int clientMin = -1;
+    int serviceMin = -1;
+    double bestZ = sol->z;
+
+    for(int i = 0; i < sol->pb->n; i++) {
+
+        for(int j = 0; j < sol->pb->m; j++) {
+
+            // vérification que la solution reste admissible
+            if(sol->capaRestantes[j] >= sol->pb->demandes[i]) {
+
+                double z = sol->z;
+                z -= sol->pb->liaisons[sol->connexionClient[i]][i];
+                z += sol->pb->liaisons[j][i];
+
+                if(sol->nbClientsServices[j] == 0) {
+                    z += sol->pb->couts[j];
+                }
+                if(sol->nbClientsServices[sol->connexionClient[i]] == 1) {
+                    z -= sol->pb->couts[sol->connexionClient[i]];
+                }
+
+                if( (clientMin == -1 || z < bestZ) && (tabuListe[i][j] <= it || z < meilleure->z) ) {
+                    bestZ = z;
+                    clientMin = i;
+                    serviceMin = j;
+                }
+
+            }
+
+        }
+
+    }
+
+    // mise à jour de la solution
+    sol->z -= sol->pb->liaisons[sol->connexionClient[clientMin]][clientMin];
+    sol->z += sol->pb->liaisons[serviceMin][clientMin];
+
+    // mise à jour du nombre de client connectés à chaque service et des services ouverts
+    sol->nbClientsServices[sol->connexionClient[clientMin]] --;
+    if(sol->nbClientsServices[sol->connexionClient[clientMin]] == 0) {
+        sol->z -= sol->pb->couts[sol->connexionClient[clientMin]];
+        sol->services[sol->connexionClient[clientMin]] = 0;
+        sol->nbServicesOuverts --;
+    }
+
+    sol->nbClientsServices[serviceMin] ++;
+    if(sol->nbClientsServices[serviceMin] == 1) {
+        sol->z += sol->pb->couts[serviceMin];
+        sol->services[serviceMin] = 1;
+        sol->nbServicesOuverts ++;
+    }
+
+    sol->capaRestantes[sol->connexionClient[clientMin]] += sol->pb->demandes[clientMin];
+    sol->capaRestantes[serviceMin] -= sol->pb->demandes[clientMin];
+
+    sol->connexionClient[clientMin] = serviceMin;
+
+    // le mouvement devient tabu
+    tabuListe[clientMin][serviceMin] = it + longueurTabu;
+
+
 }
